2fe94c42d3912176937c5880586e8ccdfa58c869
juaanhs <ruaanhs@gmail.com>
2019-08-01 16:55:46 -0300

    Refatoracao

--- app/src/main/java/br/com/juaanhs/atmconsultoria/ui/activity/MainActivity.java
+++ app/src/main/java/br/com/juaanhs/atmconsultoria/ui/activity/MainActivity.java
@@ -3,7 +3,6 @@ package br.com.juaanhs.atmconsultoria.ui.activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.view.View;
@@ -25,6 +24,11 @@ import br.com.juaanhs.atmconsultoria.ui.fragment.ServicosFragment;
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
+    public static final String ATMCONSULTORIA_EMAIL = "atmconsultoria@gmail.com";
+    public static final String ASSUNTO_EMAIL = "Contato pelo App ATM Consultoria";
+    public static final String MENSAGEM_EMAIL = "Mensagem automática";
+    public static final String KEY_EMAIL = "menssage/rfc822";
+    public static final String TITULO_ESCOLHA_O_APP = "Escolha o app de email:";
     private PrincipalFragment principalFragment;
     private ClientesFragment clientesFragment;
     private ServicosFragment servicosFragment;
@@ -35,19 +39,14 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
         instanciaFragments();
 
-        //carrega tela principal
         criaFragment(principalFragment);
+        FAB();
+        configuraNavigationDrawer(toolbar);
+    }
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                enviarEmail();
-            }
-        });
+    private void configuraNavigationDrawer(Toolbar toolbar) {
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         NavigationView navigationView = findViewById(R.id.nav_view);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -57,15 +56,25 @@ public class MainActivity extends AppCompatActivity
         navigationView.setNavigationItemSelectedListener(this);
     }
 
+    private void FAB() {
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                enviarEmail();
+            }
+        });
+    }
+
     public void enviarEmail() {
         Intent email = new Intent(Intent.ACTION_SEND);
-        email.putExtra(Intent.EXTRA_EMAIL, new String[]{"atmconsultoria@gmail.com", "ruaanhs@gmail.com"});
-        email.putExtra(Intent.EXTRA_SUBJECT, "Contato pelo App ATM Consultoria");
-        email.putExtra(Intent.EXTRA_TEXT, "Mensagem automática");
+        email.putExtra(Intent.EXTRA_EMAIL, ATMCONSULTORIA_EMAIL);
+        email.putExtra(Intent.EXTRA_SUBJECT, ASSUNTO_EMAIL);
+        email.putExtra(Intent.EXTRA_TEXT, MENSAGEM_EMAIL);
 
         //configurar apps para e-mail
-        email.setType("menssage/rfc822"); // configura o tipo e app sugestivo para email
-        startActivity( Intent.createChooser(email, "Escolha o app de email:")); // definindo Título da escolha de app
+        email.setType(KEY_EMAIL); // configura o tipo e app sugestivo para email
+        startActivity( Intent.createChooser(email, TITULO_ESCOLHA_O_APP)); // definindo Título da escolha de app
     }
 
     private void instanciaFragments() {
@@ -86,30 +95,23 @@ public class MainActivity extends AppCompatActivity
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
-
         return super.onOptionsItemSelected(item);
     }
 
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
         int id = item.getItemId();
 
         if (id == R.id.nav_principal) {
@@ -130,6 +132,7 @@ public class MainActivity extends AppCompatActivity
 
         } else if (id == R.id.nav_sobre) {
 
+            startActivity(new Intent(getApplicationContext(), SobreActivity.class));
         }
 
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
